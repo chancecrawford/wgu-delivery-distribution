7de0376f0f7ef7775c7e927d10574b449b21a013
Chance Crawford <chancecmontano@gmail.com>
2021-09-23 21:02:40 -0500

    Yes, we've had initial commit. But what about second initial commit?

---
+++ main.py
@@ -0,0 +1,16 @@
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# Main Menu options:
+# search deliveries, enter new delivery
+
+# How to do the delivery creation?
+# step-by-step enter fields, insert into hashmap
+
+# How to do the lookup?
+# ask for numbered input tied to which field to search by, get user input, search deliveries with that input
