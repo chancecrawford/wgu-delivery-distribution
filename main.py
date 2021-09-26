22cc293153ad35f3536f53254ea3be4876793226
Chance Crawford <chancecmontano@gmail.com>
2021-09-26 13:54:15 -0500

    Attempting to fix hash map, not fun

--- main.py
+++ main.py
@@ -1,30 +1,67 @@
-import logging
 import sys
 import time
 
+from package_hashtable import packages
 
-# Main Menu options:
-# search deliveries, enter new delivery
+
+# ui for searching, creating package deliveries
 def user_interface():
-    # user menu options
+    # user options: search deliveries, enter new delivery
     options = input("Enter number for desired action. \n \n"
                     "[1] Create package to deliver \n"
-                    "[2] Lookup delivery status \n \n"
+                    "[2] Lookup delivery status \n"
+                    "[3] See all packages \n \n"
                     "[0] Exit program \n"
                     )
-    user_input_actions(options)
 
-
-def user_input_actions(user_input):
-    if user_input == "0":
+    # determine actions from selection
+    # exit program
+    if options == "0":
         print("Thank you for using the WGUPS DLD scheduling system! \n \n"
               "Termination application...")
         time.sleep(2)
         sys.exit()
-    if user_input == "1":
-        print("case 1")
-    if user_input == "2":
-        print("case 2")
+    # create package delivery
+    if options == "1":
+        # get all inputs needed for inserting new package into hashtable/csv
+        package_id = input("Please enter the package id. (must be unique) \n")
+        address = input("Please enter the destination address \n")
+        city = input("Please enter the destination city. \n")
+        state = input("Please enter the destination state. \n")
+        zip_code = input("Please enter the destination zip code. \n")
+        deadline = input("Please enter the deadline the package must be delivered by. \n")
+        weight = input("Please enter the package weight. (in kilos) \n")
+        status = input("Please enter the delivery status. (at hub, delivered, en route \n")
+
+        new_package = [package_id, address, city, state, zip_code, deadline, weight, status]
+        packages.insert_package(new_package)
+
+        print('Package #', package_id, ' inserted successfully!')
+
+    # search deliveries
+    if options == "2":
+        search_results = []
+        search_choice = input("Search by package id or by delivery attribute? \n \n"
+                              "[1] Package ID \n"
+                              "[2] Delivery attribute \n")
+        # have user choose how to search for a package/delivery
+        if search_choice == "1":
+            search_id = input("Enter package id... \n")
+            search_results = packages.search_by_key(search_id)
+        if search_choice == "2":
+            search_value = input("Enter value to search for packages with. \n")
+            search_results = packages.search_by_value(search_value)
+        # print results matching search if any
+        if search_results is not None:
+            for result in search_results:
+                print(result)
+        else:
+            print("No packages found.")
+
+    if options == "3":
+        for entry in packages.map:
+            if entry is not None:
+                print(entry)
 
 
 # How to do the delivery creation?
---
+++ package_hashtable.py
@@ -0,0 +1,101 @@
+import csv
+
+
+# pull and convert package data from .csv into hash table for easier/faster reading and writing of data
+class PackageHashTable:
+
+    # create default hash table with default 40 empty entries
+    # O(1)
+    def __init__(self):
+        self.size = 40
+        self.map = [None] * self.size
+
+    def get_hash(self, key):
+        key_hash = key % self.size
+        print(key_hash)
+        return key_hash
+
+    # return list of results by value
+    # O(N^2)
+    def search_by_value(self, value_input):
+        # have to iterate through rows, then iterate through values in each row
+        result_list = []
+        for package in self.map:
+            # pass over null entries
+            if package is not None:
+                # python still thinks rows consist of None even though hash table would be populated at this point?
+                # and we're specifically saying not to search rows with None value?
+                if value_input in package[1]:
+                    result_list.append(package)
+        if result_list:
+            return result_list
+        else:
+            return None
+
+    # return package with matching key/id
+    # O(N)
+    def search_by_key(self, key_input):
+        # convert id to hash
+        key = self.get_hash(int(key_input))
+        print('search key', key)
+        # search for hash in each package
+        if self.map[key] is not None:
+            for package in self.map[key]:
+                if package[0] == key:
+                    return package[1]
+        return None
+
+    # insert package into hash table
+    # O(1)
+    def insert_package(self, key, package):
+        hash_key = self.get_hash(int(package[0]))
+        values = [key, package]
+
+        if self.map[hash_key] is None:
+            self.map[hash_key] = list(values)
+            return True
+        else:
+            for entry in self.map[hash_key]:
+                if entry[0] == key:
+                    entry[1] == package
+                    return True
+            self.map[hash_key].append(package)
+            return True
+
+
+# gets package data from .csv and inserts into hash table
+# O(N)
+def retrieve_packages(package_file):
+    package_hash_table = PackageHashTable()
+    # use csv reader to get data
+    with open(package_file) as file:
+        reader = csv.reader(file)
+        next(reader, None)
+        for row in reader:
+            package_hash_table.insert_package(row[0], row)
+    return package_hash_table
+
+
+# def search_by_value(value_input):
+#     # search every element with something like a .contains or .includes?
+#     # or get field to search by and input and search that way?
+#     # return list of results matched
+#
+#     # O(N^2) bleh
+#     result_list = []
+#     # does not work
+#     # for row in packages.table:
+#     #     if row.index(value_input):
+#     #         result_list.append(row)
+#     for row in packages.map:
+#         value_input in row:
+#         result_list.append(row)
+#
+#
+# if result_list:
+#     return result_list
+# else:
+#     return 'No packages found.'
+
+# Initialize hash table with package .csv
+packages = retrieve_packages("WGUPS Package File.csv")
